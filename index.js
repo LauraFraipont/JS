0c4f4a8525988b151aac9d2f95cd0a2de5d39f38
LauraFraipont <83577735+LauraFraipont@users.noreply.github.com>
2021-12-16 07:32:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-console.log('Hello Winc Academy');
-let name = 'laura';
-console.log('laura');
-console.log ('4+4');
+
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+
+if (age >= 18) {
+  console.log("toegang toegestaan");
+} else {
+  console.log("toegang geweigerd");
+}
+
+if (isFemale) {
+  console.log("vrouw");
+} else {
+  console.log(
+    "geen vrouw"
+  );
+}
+
+if (driverStatus === "bob") {
+  console.log("bob rijdt");
+} else {
+  console.log("geen bob, laat je rijden");
+}
