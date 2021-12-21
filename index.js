01af70ae39085d6975e170c63f3a9a47cfede96f
LauraFraipont <83577735+LauraFraipont@users.noreply.github.com>
2021-12-21 11:29:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,24 +2,47 @@
 const age = 18;
 const isFemale = true;
 const driverStatus = "bob";
+const name = "Sarah";
+const totalAmount = 27;
 
 
 if (age >= 18) {
-  console.log("toegang toegestaan");
+    console.log("Je bent 18 jaar of ouder, je mag naar binnen");
 } else {
-  console.log("toegang geweigerd");
+    console.log("je mag niet naar binnen");
+}
+
+if (age >= 18 && age <= 25) {
+    console.log("Je krijgt 50% korting op je biertje");
 }
 
 if (isFemale) {
-  console.log("vrouw");
+    console.log("vrouw");
 } else {
-  console.log(
-    "geen vrouw"
-  );
+    console.log(
+        "man, je mag helaas niet naar binnen"
+    );
 }
 
 if (driverStatus === "bob") {
-  console.log("bob rijdt");
+    console.log("Je bent de bob, je mag autorijden");
 } else {
-  console.log("geen bob, laat je rijden");
+    console.log("Je bent geen bob, je mag niet autorijden");
 }
+
+if (name === "Sarah" || name === "Bram") {
+    console.log("Je naam is Sarah of Bram, je krijgt een gratis biertje!");
+} else {
+    console.log("Je doet niet mee aan onze actie");
+}
+
+if (totalAmount >= 100) {
+    console.log("gratis flesje champagne.");
+} else if (totalAmount > 50) {
+    console.log("gratis portie nachos");
+} else if (totalAmount > 25) {
+    console.log("gratis bitterballen");
+} else {
+    console.log("Helaas geen korting");
+}
+
